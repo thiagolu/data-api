7412fdf99a0296a8b7d25dc28c41b964f6fba04a
thiagolu <tlusilva05@gmail.com>
2019-02-25 01:16:24 -0300

    cors and frontend csv upload

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::API
+  before_action :validate_api_request
+
+  API_TOKEN = '123qwe'
+
+  def import
+    render status: 200, json: { it_works: params[:file].read }
+  end
+  
+  
+  def status
+    render status: 200, json: { it_works: 'works' }
+  end
+  
+  private
+
+  def validate_api_request
+    return render status: 500 unless API_TOKEN == request.headers['Authorization']
+  end
 end
